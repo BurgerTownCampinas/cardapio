08afc237f9f326c26c24d0cf1ff4f70aecfdd58c
BurgerTownCampinas <74783345+BurgerTownCampinas@users.noreply.github.com>
2020-11-21 15:43:18 -0300

    Add files via upload

--- js/helper.js
+++ js/helper.js
@@ -1,9 +1,7 @@
 class Helper{
     mensagemDeliveryWhatsApp(telefone, tipoProduto, idproduto){
-        var mensagemComum = "Olá BurgerTown gostaria de realizar o pedido do lanche " + tipoProduto + "." +
-                            " Meu nome é: [Coloque seu nome aqui]" +
-                            " Entregar no endereço: [Coloque aqui seu endereço]";
-
+        var mensagemComum = "Olá BurgerTown gostaria de realizar o pedido do lanche " + tipoProduto + "."
+                            
         idproduto.href = "https://api.whatsapp.com/send?phone=" + telefone + "&text=" + mensagemComum;
     }
 
